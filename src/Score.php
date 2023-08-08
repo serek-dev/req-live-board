e938c6e1e7fcb25627a624a0d00560ba9f64ea21
Sebastian Twarog <sebastian.twarog1989@gmail.com>
2023-08-08 21:35:44 +0200

    Score should not accept negative numbers

--- src/Score.php
+++ src/Score.php
@@ -6,12 +6,16 @@ declare(strict_types=1);
 namespace Scalo\Task;
 
 
+use InvalidArgumentException;
+
 final class Score
 {
     public function __construct(
-        private readonly int $tomeTeam,
+        private readonly int $homeTeam,
         private readonly int $awayTeam,
-    )
-    {
+    ) {
+        if ($this->homeTeam < 0 || $this->awayTeam < 0) {
+            throw new InvalidArgumentException('Score must be equal or greater than 0');
+        }
     }
 }
--- tests/Unit/ScoreTest.php
+++ tests/Unit/ScoreTest.php
@@ -2,8 +2,9 @@
 
 namespace Unit\Scalo\Task;
 
-use Scalo\Task\Score;
+use InvalidArgumentException;
 use PHPUnit\Framework\TestCase;
+use Scalo\Task\Score;
 
 /** @covers \Scalo\Task\Score */
 final class ScoreTest extends TestCase
@@ -12,5 +13,29 @@ final class ScoreTest extends TestCase
     {
         $sut = new Score(0, 0);
         $this->assertInstanceOf(Score::class, $sut);
+    }
+
+    /** @dataProvider providerForConstructorFailsOnNegativeNumbers */
+    public function testConstructorFailsOnNegativeNumbers(int $home, int $away): void
+    {
+        $this->expectException(InvalidArgumentException::class);
+        new Score($home, $away);
+    }
+
+    /**
+     * @return array<string, array<string, int>>
+     */
+    public function providerForConstructorFailsOnNegativeNumbers(): array
+    {
+        return [
+            'home team is negative' => [
+                'home' => -1,
+                'away' => 0,
+            ],
+            'away team is negative' => [
+                'home' => 0,
+                'away' => -1,
+            ],
+        ];
     }
 }
